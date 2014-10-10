11e9fe28bfbabafc1891b62b5f392bc7c255d948
Karissa McKelvey <krmckelv@gmail.com>
2014-10-10 14:45:28 -0700

    CLN: remove routes.js cruft

--- routes.js
+++
@@ -1,7 +0,0 @@
-var http = require('http')
-var debug = require('debug')('index')
-
-function Routes(req) {
-  if (req.url.match(/login/)) return github.login(req, res)
-  if (req.url.match(/callback/)) return github.callback(req, res)
-}
