8d8461d5917c6bc05ea1077a1b8d4122f40ad67e
Karissa McKelvey <krmckelv@gmail.com>
2014-11-19 10:33:52 -0600

    first pass at publishing

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,24 +1,28 @@
 var user = require('../models/user.js');
 var debug = require('debug')('profile')
 
-module.exports = function () {
-  var ractive = this
+module.exports = {
+  data: {},
+  template: require('../templates/pages/profile.html'),
+  onrender: function () {
+    var ractive = this
 
-  ractive.on('submit', function (event) {
-    user.update(ractive.get('user'), function (err) {
-      var message = {
-        'type': 'success',
-        'text': 'Profile updated successfully!'
-      }
-
-      if (err) {
-        message = {
-          'type': 'error',
-          'text': 'We could not update your profile!'
+    ractive.on('submit', function (event) {
+      user.update(ractive.get('user'), function (err) {
+        var message = {
+          'type': 'success',
+          'text': 'Profile updated successfully!'
         }
-      }
-      window.ractive.set('message', message)
-    })
-    event.original.preventDefault();
-  });
+
+        if (err) {
+          message = {
+            'type': 'error',
+            'text': 'We could not update your profile!'
+          }
+        }
+        window.ractive.set('message', message)
+      })
+      event.original.preventDefault();
+    });
+  }
 }
