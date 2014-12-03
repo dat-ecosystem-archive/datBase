0e8ff45404d3499ff6fa19eae647d8c174d1a5fc
Karissa McKelvey <krmckelv@gmail.com>
2014-12-03 10:56:14 -0800

    Add some debug statements

--- app/common/error-request.js
+++ app/common/error-request.js
@@ -1,6 +1,8 @@
 var xhr = require('xhr')
+var debug = require('debug')('request')
 
 module.exports = function (options, callback) {
+  debug('requesting', options)
   xhr(options, function (err, resp, json) {
     var error;
     if (json.status == 'error') {
@@ -15,6 +17,7 @@ module.exports = function (options, callback) {
       })
     }
     else {
+      debug('response', err, resp, json)
       return callback(err, resp, json)
     }
   })
