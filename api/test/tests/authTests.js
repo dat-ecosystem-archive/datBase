03fe4f7e0bfd751a3bc6363ae69ac282e4f97477
Karissa McKelvey <krmckelv@gmail.com>
2014-12-10 13:25:33 -0800

    TST: simplify the auth test a little

--- api/test/tests/authTests.js
+++ api/test/tests/authTests.js
@@ -1,23 +1,20 @@
 var request = require('request')
 var jar = request.jar()
 
-
-var req = function(path, port, cb) {
-  request({
-    url: 'http://localhost:' + port + path,
-    jar: jar,
-    json: true
-  }, cb)
-}
 module.exports.loginUser = function(test, common) {
   test('login a user', function(t) {
     common.testGET(t, '/auth/login',
       function (err, api, res, json, done) {
         t.ifError(err)
-        req('/auth/currentuser', api.options.PORT, function (err, res, data) {
+        request({
+          url: 'http://localhost:' + api.options.PORT + "/auth/currentuser",
+          jar: jar,
+          json: true
+        },  function (err, res, data) {
           var c = jar.getCookies('http://localhost')[0];
-          t.ok(c)
-          t.equals(c.key, 'dat-registry')
+          t.ok(c, 'jar here')
+          t.equals(c.key, 'dat-registry', 'key is set')
+          t.ok(c.value, 'value is set')
           done()
         })
       }
