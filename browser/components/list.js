c7e640b725d0227edcca86e9c011ee3fe330920d
Karissa McKelvey <krmckelv@gmail.com>
2015-01-24 13:51:47 -0800

    Comment out some part of the metadat list component

--- browser/components/list.js
+++ browser/components/list.js
@@ -5,8 +5,9 @@ var MetadatList = Ractive.extend({
   template: require('./list.html'),
   onrender: function () {
     var ractive = this
-    this.on('toggleMetadat', function (metadat) {
-      metadat.closed = !metadat.closed
+    this.on('toggleMetadat', function (event) {
+      var metadat = event.context
+      metadat.open = !metadat.open
     });
   },
   data: { metadats: [] }
