d0843dac36b529cd74b52793a87f66d5d8defe57
Max Ogden <max@maxogden.com>
2015-01-29 14:35:16 -0800

    add comments to indexer.js

--- api/indexer.js
+++ api/indexer.js
@@ -5,6 +5,13 @@ var changeProcessor = require('level-change-processor')
 var indexer = require('level-indexer')
 var runSeries = require('run-series')
 
+// high level description:
+// when data is stored in a model,
+// for every property in that model's schema that needs indexing,
+// store two indexes,
+// a secondary index with key=property and value=primaryKey,
+// a reverse index with key=primaryKey and value=property.
+
 module.exports = function(opts) {
   var indexers = {}
   
@@ -13,19 +20,24 @@ module.exports = function(opts) {
     // only create indexers for properties marked
     // as 'index: true' in the schema
     if (!prop.index) return
+      
+    // the custom map function here fetches the entire row
+    // because only the key is stored in the index
     var indexDb = indexers[key] = indexer(opts.db, key, {
       map: function (key, cb) {
         opts.model.db.get(key, cb)
       }
     })
 
+    // listen to changes on the model
     var processor = changeProcessor({
-      db: opts.state,
+      db: opts.state, // db to store change processor state in
       feed: opts.feed,
       worker: worker,
-      key: 'latest-' + key
+      key: 'latest-' + key // prefix because all processors share one state db
     })
     
+    // used to check if we have an index for a key
     var reverseIndex = subleveldown(opts.db, 'reverse-' + key)
     
     function worker(change, cb) {
@@ -78,9 +90,12 @@ module.exports = function(opts) {
       }
       
       function deleteCurrentIndex(key, cb) {
+        // first check if we have an index for this key
         reverseIndex.get(key, function(err, currentIndexKey) {
           if (err && !err.notFound) return cb(err)
+          // if not we can skip the rest of this function
           if (err && err.notFound) return cb()
+          // if we do then we have to delete both our index and our reverse index
           opts.db.del(currentIndexKey, function(err) {
             if (err) return cb(err)
             reverseIndex.del(key, function(err) {
@@ -92,6 +107,7 @@ module.exports = function(opts) {
       }
       
       function storeNewIndex(key, val, index, cb) {
+        // store our index (keyed by `val`) and our reverse index (keyed by `key`)
         indexDb.add(val, key, function(err) {
           if (err) return cb(err)
           reverseIndex.put(key, index, cb)
