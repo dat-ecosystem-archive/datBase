2ca5324a08e25aa7a64a8e5d64e51721a422a740
Karissa McKelvey <krmckelv@gmail.com>
2015-09-15 22:20:17 -0700

    update server to get port

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var createServer = require('./')
 var fs = require('fs')
+var getport = require('get-port')
 
 var config = JSON.parse(fs.readFileSync('./config.json').toString())
 
 var opts = {
   GITHUB_CLIENT: config.GITHUB_CLIENT || process.env['GITHUB_CLIENT'],
   GITHUB_SECRET: config.GIHUB_SECRET || process.env['GITHUB_SECRET'],
-  PORT: config.PORT || process.env['PORT'] || 5000,
-  admins: config.admins
+  PORT: config.PORT || process.env['PORT'],
+  CREATE_DAT: config.CREATE_DAT || process.env['CREATE_DAT']
 }
 
 var api = createServer(opts)
-var port = api.options.PORT
+getport(5000, function (err, port) {
 
-api.server.listen(port, function() {
-  console.log('listening on port', port)
+  var port = api.options.PORT || port
+
+  api.server.listen(port, function (err) {
+    console.log('listening on port', port)
+  })
+
 })
