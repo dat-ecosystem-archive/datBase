b071255f505d07f2650c26bcada83235f47d2908
Karissa McKelvey <krmckelv@gmail.com>
2016-01-29 18:45:10 -0500

    make sure to skip creation if table exists

--- api/db/index.js
+++ api/db/index.js
@@ -6,16 +6,19 @@ module.exports = function (opts, cb) {
     debug: opts.debug,
     connection: opts.connection
   })
+  knex.schema.hasTable('users').then(function (exists) {
+    if (exists) return cb(null, knex)
 
-  knex.schema.createTableIfNotExists('users', function (table) {
-    table.uuid('id').primary().unique()
-    table.string('nickname')
-    table.string('email')
-    table.string('hash')
-    table.string('salt')
-    table.boolean('verified')
-  }).asCallback(function (err) {
-    if (err) return cb(err)
-    return cb(null, knex)
+    knex.schema.createTable('users', function (table) {
+      table.uuid('id').primary().unique()
+      table.string('nickname')
+      table.string('email')
+      table.string('hash')
+      table.string('salt')
+      table.boolean('verified')
+    }).asCallback(function (err) {
+      if (err) return cb(err)
+      return cb(null, knex)
+    })
   })
 }
