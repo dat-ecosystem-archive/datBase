68eab26e776ddc348843ce7c669a6215bdc4e85e
Lauren Garcia <lauren.garcia@gmail.com>
2016-06-16 15:02:37 -0700

    minidux+yo-yo components
    rm component-level .less file
    
    componentize peers
    
    forgot to git add components/peers
    
    speed display component (rough)
    
    remove attachSpeedometer() from index.js
    
    debug notes for tomorrow
    
    wire up minidux reducers properly
    
    debug hyperdrive size on reset
    
    BUG: only one master minidux subscriber allowed, todo: refactor this with that knowledge
    
    refactored store pub/sub for HyperdriveSize component
    
    update ResetButton components
    
    notes
    
    add Peers back into the mix
    
    fix edge case of peers = 0 hen last one drops off
    
    cleanup
    
    remove consoles
    
    add speedometer back into the mix
    
    remove consoles
    
    action  ->
    
    SpeedDisplay component _calculate method
    
    hyperdrive size component pseudo-privatize props
    
    peers components privatize props
    
    componentify Help/Intro
    
    cleanup
    
    define default states for reducers
    
    todo for tomorrow
    
    one speedometer per direction

---
+++ components/hyperdrive-size/index.js
@@ -0,0 +1,37 @@
+var yo = require('yo-yo')
+var prettyBytes = require('pretty-bytes')
+
+module.exports = HyperdriveSize
+
+function HyperdriveSize (el) {
+  if (!(this instanceof HyperdriveSize)) return new HyperdriveSize(el)
+  var self = this
+  this.$el = document.getElementById(el)
+  this._component = this._render()
+  this._size = 0
+
+  if (this.$el) this.$el.appendChild(this._component)
+}
+
+HyperdriveSize.prototype.update = function (state) {
+  if (state && state.archiveReducer) {
+    this._updateSize(state)
+    yo.update(this._component, this._render())
+  }
+}
+
+HyperdriveSize.prototype._render = function () {
+  var size = this._size
+  var component = yo`<p id="size">Drive size: ${size}</p>`
+  return component
+}
+
+HyperdriveSize.prototype._updateSize = function (state) {
+  if (state.archiveReducer) {
+    var s = state.archiveReducer
+    if (s.archive &&  s.archive.content && s.archive.content.bytes) {
+      return this._size = prettyBytes(s.archive.content.bytes)
+    }
+    else { return this._size = 0 }
+  }
+}
---
+++ components/peers/index.js
@@ -0,0 +1,34 @@
+var yo = require('yo-yo')
+
+module.exports = Peers
+
+function Peers (el) {
+  if (!(this instanceof Peers)) return new Peers(el)
+  var self = this
+  this.$el = document.getElementById(el)
+  this._component = this._render()
+  this._peers = 0;
+
+  if (this.$el) this.$el.appendChild(this._component)
+}
+
+Peers.prototype.update = function (state) {
+  if (state && state.peersReducer) {
+    this._updateNumPeers(state)
+    yo.update(this._component, this._render())
+  }
+}
+
+Peers.prototype._render = function () {
+  var peers = this._peers || 0
+  var pl = peers > 1 || peers === 0 ? 's' : ''
+  var component = yo`<span>${peers} Source${pl}</span>`
+  return component
+}
+
+Peers.prototype._updateNumPeers = function (state) {
+  if (state.peersReducer && state.peersReducer.peers) {
+    return this._peers = state.peersReducer.peers
+  }
+  else { return this._peers = 0 }
+}
