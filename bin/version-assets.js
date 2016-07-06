83611d3e6f33b514042e72e184d423d1dea96a4a
Lauren Garcia <lauren.garcia@gmail.com>
2016-07-05 19:26:11 -0700

    update version-assets script to account for minification step

--- bin/version-assets.js
+++ bin/version-assets.js
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
-
+var fs = require('fs')
 var Version = require('node-version-assets')
 
-var versionAssets = new Version({
+// productionize `index.html` by updating asset references
+// `main.css` -> 'main.min.css'
+// `app.js` -> `app.min.js`
+// copy original, un-updated `index.html` to `debug.html`
+var index = fs.readFileSync('index.html', 'utf8')
+fs.renameSync('index.html', 'debug.html')
+var prod = index
+             .replace(/public\/css\/main.css/gmi, 'public/css/main.min.css')
+             .replace(/public\/js\/app.js/gmi, 'public/css/app.min.js')
+fs.writeFileSync('index.html', prod, 'utf8')
+
+// version `app.min.js` and `main.min.js` by appending md5 hash to filename
+// & update references to files in `index.html`
+var versionInstance = new Version({
   assets: [
-    'public/js/app.js',
-    'public/css/main.css'
+    'public/js/app.min.js',
+    'public/css/main.min.css'
   ],
   grepFiles: [
     'index.html'
@@ -14,4 +27,4 @@ var versionAssets = new Version({
   keepOldVersions: false
 })
 
-versionAssets.run()
+versionInstance.run()
