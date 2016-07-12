71793f914dcf11d735aae3996a93160e280c803b
Lauren Garcia <lauren.garcia@gmail.com>
2016-07-11 17:21:34 -0700

    refine git-sha.js

--- bin/git-sha.js
+++ bin/git-sha.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 var fs = require('fs')
 
-var file = 'REVISION' // shipit generates this file
+var file = 'REVISION' // shipit generates this file, uses it for rollbacks, etc
 fs.stat(file, function (err, stats) {
   var sha
   var print
   if (err) { sha = 'error' }
   if (stats && stats.isFile()) {
-    sha = fs.readFileSync(file)
+    sha = fs.readFileSync(file, 'utf-8')
   } else {
     sha = 'error'
   }
-  print = 'git sha: ' + sha
-  fs.writeFileSync('version.txt', print, 'utf8')
-  console.log('Git sha written to version.txt:\n  ' + print)
+  print = '{ git_sha: "' + sha.toString('utf-8').split('\n')[0] + '" }'
+  fs.writeFileSync('version.json', print, 'utf8')
+  console.log('git sha written to version.json:\n  ' + print)
 })
