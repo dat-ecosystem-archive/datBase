0287f6cdc2cd68adc02e5a9603f446076821d86e
Kristina Schneider <me@kristinaschneider.com>
2016-07-27 18:14:15 +0200

    Style dat details header (#156)
    * group dat details into dat details header
    stick status bar to the bottom
    
    * style share-link
    
    * style details in dat details header
    
    * clean up confusing css class naming
    
    * move site-header and dat-header styles into separate .scss files
    
    * delete unused styles
    
    * move file-system and file-queue styles into separate scss files
    
    * move breakpoint helpers into separate scss file
    
    * namespace table styles used for displaying data
    
    * move status bar styles into separate scss file
    
    * move dat speed styles into dat-details file.
    use class instead of ID for lower specificity.
    
    * move styles for previewing file content into separate scss file
    
    * improve layout on small screens:
    - add viewport
    - add breakpoints to container class
    - adjust layout of header elements
    
    * avoid speed info in dat-header wrapping to next line on small screens
    
    * layout fix for footer status bar
    
    * remove extra margin
    
    * add placeholder buttons for "Download Dat" and "Open in Desktop App"
    
    * add a min-width of 480px to the body (only necessary as long as layout is not optimized for smaller screens)
    
    * style add files button

--- src/js/components/add-files/index.js
+++ src/js/components/add-files/index.js
@@ -3,5 +3,5 @@ var yo = require('yo-yo')
 module.exports = function AddFiles (props) {
   this.props = props
 
-  return yo`<label><div style="display: inline; padding: 0.2em; color: white; text-transform: uppercase; background-color: #2980b9">Add Files</div><input multiple style="display: none" type="file" class="dat-button" onchange=${this.props.onfiles} /></label>`
+  return yo`<label><div class="button-add">Add Files</div><input multiple style="display: none" type="file" class="dat-button" onchange=${this.props.onfiles} /></label>`
 }
