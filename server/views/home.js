b8cf665611de321828a0ca101d3ff6fd0127ec49
Lauren Garcia <lauren.garcia@gmail.com>
2016-07-28 18:51:14 -0400

    update npm run dev to watch server with nodemon

--- server/views/home.js
+++ server/views/home.js
@@ -1,9 +1,8 @@
 const html = require('choo/html')
 
 const homeView = (state, prev, send) => {
-  return html`<div class="tmp-home-view"
-        style="background: yellow; padding: 50px;">
-      <h1>Home View: ${state.home.h1}</h1>
+  return html`<div class="tmp-home-view">
+      <h1>Home View Test: ${state.home.h1}</h1>
       <ul>
         <li>What is Dat.land?</li>
         <li>How can I use it?</li>
