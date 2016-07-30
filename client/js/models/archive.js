ed87d8bf73421220a6ca400645395f8f6cfd9562
Lauren Garcia <lauren.garcia@gmail.com>
2016-07-30 00:50:54 -0400

    strip out old hash-based logic for resolving archive key

--- client/js/models/archive.js
+++ client/js/models/archive.js
@@ -1,24 +1,17 @@
 module.exports = {
   namespace: 'archive',
   state: {
-    key: _resolveLocationData().key,
-    file: _resolveLocationData().file
+    key: null,
+    file: null
   },
   reducers: {
     update: (data, state) => {
       return {
-        archiveKey: _resolveLocationData().key,
-        file: _resolveLocationData().file
+        key: null,
+        file: null
       }
     }
   }
 }
 
-function _resolveLocationData (data, state) {
-  const keypath = window.location.hash.substr(1).match('([^/]+)(/?.*)')
-  return {
-    key: keypath ? keypath[1] : null,
-    file: keypath ? keypath[2] : null
-  }
-}
 
