2b12dd497d1550d1294f8fad587d3122c093936b
Chia-liang Kao <clkao@clkao.org>
2016-08-03 11:45:06 +0800

    first cut of header migration

--- server/components/header/index.js
+++ server/components/header/index.js
@@ -1,7 +1,27 @@
 const html = require('choo/html')
+var button = require('dat-button')
+var importButton = () => 'import button'
+var help = () => 'help'
 
 const header = (state, prev) => {
-  return html`<div id="header-tmp">TODO: migrate header</div`
+  return html`<header class="site-header">
+    <div class="container">
+      <a href="http://dat-data.com" class="dat-logo">
+        <img src="./public/img/dat-data-logo.svg" />
+      </a>
+      <div class="site-header__actions">
+        <div class="dat-button dat-button--new-dat">
+          ${button({
+            text: 'Create new Dat'
+          })}
+        </div>
+        ${importButton({
+          download: 'zzz'
+        })}
+        ${help()}
+      </div>
+    </div>
+  </header>`
 }
 
 module.exports = header
