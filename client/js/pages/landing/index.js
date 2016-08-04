8dcfbb8fb661f0735514e31feeaffb0a0c08a7a0
Lauren Garcia <lauren.garcia@gmail.com>
2016-08-05 01:53:40 +0200

    remove unnecessary temp home/landing page container div

--- client/js/pages/landing/index.js
+++ client/js/pages/landing/index.js
@@ -9,25 +9,23 @@ const landingPage = (state, prev) => {
     </head>
     <body>
       ${header(state, prev)}
-      <div class="tmp-home-view">
-        <div class="landing-header">
-          Dat is a dataset sharing system.
-          <br>
-          Give it a try:
+      <div class="landing-header">
+        Dat is a dataset sharing system.
+        <br>
+        Give it a try:
+      </div>
+      <div class="landing-main container">
+
+        <div class="landing-create-new-dat">
+          <h3>Create New Dat</h3>
+          <p>
+            Drag and drop files to upload and start sharing your data
+          </p>
         </div>
-        <div class="landing-main container">
 
-          <div class="landing-create-new-dat">
-            <h3>Create New Dat</h3>
-            <p>
-              Drag and drop files to upload and start sharing your data
-            </p>
-          </div>
-
-          <div class="landing-import-dat">
-            <h3>Or Open An Existing Dat</h3>
-            <input />
-          </div>
+        <div class="landing-import-dat">
+          <h3>Or Open An Existing Dat</h3>
+          <input />
         </div>
       </div>
     </body></html>`
