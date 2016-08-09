1e018ecfda3d2157dbf9f91e7ca9893caf6267e6
karissa <krmckelv@gmail.com>
2016-08-09 18:14:41 +0200

    get archive in another file

--- client/js/components/hyperdrive/index.js
+++ client/js/components/hyperdrive/index.js
@@ -2,21 +2,8 @@ const html = require('choo/html')
 var hyperdriveUI;
 if (!module.parent) hyperdriveUI = require('hyperdrive-ui')
 const path = require('path')
-const hyperdrive = require('hyperdrive')
 const pretty = require('pretty-bytes')
-const memdb = require('memdb')
-const swarm = require('hyperdrive-archive-swarm')
-
-function listItem (entry) {
-  // XXX: this is copied from yo-fs.
-  return html`<li class='entry ${entry.type}'>
-      <a href="javascript:void(0)">
-        <span class="name">${path.basename(entry.name)}</span>
-        <span class="modified">${entry.mtime ? relative(entry.mtime) : ''}</span>
-        <span class="size">${pretty(entry.length)}</span>
-      </a>
-    </li>`
-  }
+const getArchive = require('./archive.js')
 
 module.exports = function (state, prev, send) {
   if (module.parent) {
@@ -35,8 +22,18 @@ module.exports = function (state, prev, send) {
   }
 
   // dynamic hyperdrive view using discovery-swarm
-  var drive = hyperdrive(memdb())
-  var archive = drive.createArchive(state.archive.key)
-  var sw = swarm(archive)
+  var archive = getArchive(state.archive.key)
   return hyperdriveUI(archive, {entries: state.archive.entries})
+}
+
+
+function listItem (entry) {
+  // XXX: this is copied from yo-fs.
+  return html`<li class='entry ${entry.type}'>
+      <a href="javascript:void(0)">
+        <span class="name">${path.basename(entry.name)}</span>
+        <span class="modified">${entry.mtime ? relative(entry.mtime) : ''}</span>
+        <span class="size">${pretty(entry.length)}</span>
+      </a>
+    </li>`
 }
