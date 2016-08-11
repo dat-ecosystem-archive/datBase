ae9739ef4e659ee5a11cc0b668f13487c80407d7
Lauren Garcia <lauren.garcia@gmail.com>
2016-08-12 00:22:24 +0200

    bug: send state.archive to server-side hyperdrive list renderer

--- client/js/components/hyperdrive/index.js
+++ client/js/components/hyperdrive/index.js
@@ -1,24 +1,23 @@
 const html = require('choo/html')
-// const serverComponents = process.browser ? {} : require('./../../app.js').serverComponents
-// const hyperdriveRenderer = process.browser ? require('hyperdrive-ui') : serverComponents.hyperdrive
 const path = require('path')
 const pretty = require('pretty-bytes')
 const getArchive = require('./archive.js')
 
 let hyperdriveRenderer
 if (!module.parent) {
+  console.log('CLIENT')
   hyperdriveRenderer = require('hyperdrive-ui')
 } else {
-  const app = require('./../../app.js')
-  const serverComponents = app.serverComponents || {}
+  console.log('SERVER')
+  const serverComponents = require('./../../app.js').serverComponents || {}
   hyperdriveRenderer = serverComponents.hyperdrive
 }
 
 
 module.exports = function (state, prev, send) {
-  if (process.browser) {
+  if (module.parent) {
     // XXX: static rendering of hyperdrive list from server side state
-    return hyperdriveRenderer(state)
+    return hyperdriveRenderer(state.archive)
   }
   // XXX: dynamic hyperdrive view using discovery-swarm
   let archive = getArchive(state.archive.key)
