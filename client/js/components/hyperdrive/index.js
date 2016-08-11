42121930d438c21fb514ff27faf4c66b644537e1
Lauren Garcia <lauren.garcia@gmail.com>
2016-08-12 00:33:58 +0200

    prettier hyperdriveRenderer logic

--- client/js/components/hyperdrive/index.js
+++ client/js/components/hyperdrive/index.js
@@ -2,24 +2,17 @@ const html = require('choo/html')
 const path = require('path')
 const pretty = require('pretty-bytes')
 const getArchive = require('./archive.js')
-
-let hyperdriveRenderer
-if (!module.parent) {
-  console.log('CLIENT')
-  hyperdriveRenderer = require('hyperdrive-ui')
-} else {
-  console.log('SERVER')
-  const serverComponents = require('./../../app.js').serverComponents || {}
-  hyperdriveRenderer = serverComponents.hyperdrive
-}
-
+var hyperdriveRenderer
 
 module.exports = function (state, prev, send) {
   if (module.parent) {
+    const serverComponents = require('./../../app.js').serverComponents || {}
+    hyperdriveRenderer = serverComponents.hyperdrive
     // XXX: static rendering of hyperdrive list from server side state
     return hyperdriveRenderer(state.archive)
   }
   // XXX: dynamic hyperdrive view using discovery-swarm
+  hyperdriveRenderer = require('hyperdrive-ui')
   let archive = getArchive(state.archive.key)
   return hyperdriveRenderer(archive, {entries: state.archive.entries})
 }
