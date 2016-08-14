558eb26acb23af444f634bdc3c60ef2566f42a73
Chia-liang Kao <clkao@clkao.org>
2016-08-14 22:59:43 +0800

    styling the stub

--- client/js/pages/archive/index.js
+++ client/js/pages/archive/index.js
@@ -9,26 +9,44 @@ const archivePage = (state, prev, send) => {
   return html`
     <div>
       ${header(state, prev, send)}
-      <div class="archive-metadata">
-        <h1>ArchivePage</h1>
-        <h2>Server-rendered properties:</h2>
-        ${error(state.archive.error)}
-        <ul>
-        <li>archive key: ${state.archive.key}</li>
-        <li>peers: ${state.archive.numPeers}</li>
-        <li>${permissions(state, prev, send)}</li>
-        <li>signalhubs:
+      <div id="dat-info" class="dat-header">
+        <div class="container">
+          ${error(state.archive.error)}
           <ul>
-          ${state.archive.signalhubs.map(function (fqdn) {
-            return signalhubs(fqdn)
-          })}
+          <li>archive key: ${state.archive.key}</li>
+          <li>signalhubs:
+            <ul>
+            ${state.archive.signalhubs.map(function (fqdn) {
+              return signalhubs(fqdn)
+            })}
+            </ul>
+          </li>
           </ul>
-        </li>
-        </ul>
+          <div class="dat-details">
+            <div id="permissions" class="dat-detail">${permissions(state, prev, send)}</div>
+            <div id="hyperdrive-size" class="dat-detail"><p id="size">92.78 kB</p></div>
+            <div id="peers" class="dat-detail">${state.archive.numPeers} Source(s)</div>
+            <div style="display: none;" id="speed" class="dat-detail dat-detail--speed"><div>
+              <span id="download-speed"></span> / <span id="upload-speed"></span>
+            </div></div>
+          </div>
+        </div>
       </div>
-      <main id="archive-list" class="container">
-        ${hyperdrive(state, prev, send)}
+      <main id="site-main">
+        <div class="container">
+          <div id="add-files">XXX: add files</div>
+          <div id="file-queue">XXX: file queue</div>
+          ${hyperdrive(state, prev, send)}
+        </div>
       </main>
+      <div class="status-bar">
+        <div class="container">
+          <span id="help-text" class="status-bar-status"></span>
+          <div style="display: block;" id="hyperdrive-stats" class="status-bar-stats">
+            <span>Total: XX kB / XXX kB</span>
+          </div>
+        </div>
+      </div>
     </div>`
 }
 
