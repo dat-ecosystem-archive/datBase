baa638aa4194fa58ed0d83f5ea9416838088124d
Kristina Schneider <me@kristinaschneider.com>
2016-08-26 17:49:32 +0200

    style previewing a file in a dat datproject/design#17

--- client/js/components/preview/index.js
+++ client/js/components/preview/index.js
@@ -5,15 +5,30 @@ const preview = (state, prev, send) => {
   const isOpen = state.preview.isPanelOpen ? 'open' : ''
   const fileName = state.preview.fileName
   return html`<section id="preview" class="panel ${isOpen}">
-    <div class="panel-contents">
-      ${fileName}
+    <div class="panel-header">
       ${button({
-        klass: 'close-panel',
-        text: 'close',
+        klass: 'btn--green panel-header__close-button',
+        text: 'Close',
         click: () => {
           send('preview:closePanel')
         }
       })}
+      <div class="panel-header__title-group">
+        <div class="panel-title">
+          ${fileName}
+        </div>
+        <div class="dat-details">
+          <div class="dat-detail">XX.X KB</div>
+          <div class="dat-detail">some other metadata</div>
+        </div>
+      </div>
+      <div class="panel-header__action-group">
+        <button class="dat-header-action">Download</button>
+        <button class="dat-header-action">Open in Desktop App</button>
+      </div>
+    </div>
+    <div class="panel-main">
+      [XXX preview content goes here]
     </div>
   </preview>`
 }
