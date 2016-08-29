92fd22a2a6d8e5e5f4457c00e6f3211bf0fb68bd
Lauren Garcia <lauren.garcia@gmail.com>
2016-08-29 18:48:22 +0200

    move progress listener out of choo model since redraws are batched

--- client/js/components/import-queue/index.js
+++ client/js/components/import-queue/index.js
@@ -2,15 +2,22 @@ const html = require('choo/html')
 
 module.exports = (state, prev, send) => {
   const writing = state.archive.importQueue.writing
-  const writingProgressPct = state.archive.importQueue.writingProgressPct
+  const prevWriting = prev && prev.archive ? prev.archive.importQueue.writing : null
   const next = state.archive.importQueue.next
+
+  if ((writing && !prevWriting) ||
+      (writing && prevWriting && writing.fullPath !== prevWriting.fullPath)) {
+    writing.progressHandler = _progressHandler
+    writing.progressListener.on('progress', writing.progressHandler)
+  }
+  // TODO: remove progress handler onFileWriteComplete
 
   return _render()
 
   function _render () {
     if (writing || next.length > 0) {
       return html`<ul id="file-queue">
-        ${writing ? _renderLi(writing, writingProgressPct) : undefined}
+        ${writing ? _renderLi(writing) : null}
         ${next.map(function (file) {
           return _renderLi(file)
         })}
@@ -20,21 +27,21 @@ module.exports = (state, prev, send) => {
     }
   }
 
-  function _renderLi (file, progressPct) {
+  function _renderLi (file) {
     return html`<li>
       ${file.fullPath}
-      ${_renderProgress(file, progressPct)}
+      ${_renderProgress(file)}
     </li>`
   }
 
-  function _renderProgress (file, progressPct) {
+  function _renderProgress (file) {
     if (file.writeError) {
       return html`<div class="progress error">Error</div>`
     }
     let loaded = 0
     let klass = 'progress__line--loading'
-    if (progressPct) loaded = progressPct
-    if (progressPct === 100) klass = 'progress__line--complete'
+    // if (progressPct) loaded = progressPct
+    // if (progressPct === 100) klass = 'progress__line--complete'
     return html`<div class="progress">
        <div class="progress__counter">${loaded}%</div>
        <div class="progress__bar">
@@ -43,5 +50,11 @@ module.exports = (state, prev, send) => {
          </div>
        </div>
      </div>`
+  }
+
+  function _progressHandler (progress) {
+    const pct = parseInt(progress.percentage)
+    console.log(pct)
+    // TODO: update element
   }
 }
