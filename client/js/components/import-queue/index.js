e527eb4ecd086a31561d08bc8cb1c5588d3f5560
Lauren Garcia <lauren.garcia@gmail.com>
2016-09-01 03:08:47 +0200

    standard

--- client/js/components/import-queue/index.js
+++ client/js/components/import-queue/index.js
@@ -2,14 +2,13 @@ const html = require('choo/html')
 
 module.exports = (state, prev, send) => {
   const writing = state.archive.importQueue.writing
-  const prevWriting = prev && prev.archive ? prev.archive.importQueue.writing : null
   const next = state.archive.importQueue.next
 
   if (writing && !writing.progressHandler) {
     writing.progressPct = 0
     writing.progressHandler = (progress) => {
       const pct = parseInt(progress.percentage)
-      send('archive:updateImportQueue', {writingProgressPct: pct, writing: writing}, function(){})
+      send('archive:updateImportQueue', {writingProgressPct: pct, writing: writing}, function () {})
     }
     writing.progressListener.on('progress', writing.progressHandler)
   }
