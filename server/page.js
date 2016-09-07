21ecdf6382fe1cc7e4fe6a94c0b003658710e173
Karissa McKelvey <karissa@users.noreply.github.com>
2016-09-07 13:05:21 +0200

    add browser warning for incompatible browsers (#218)

--- server/page.js
+++ server/page.js
@@ -6,11 +6,19 @@ function page (contents, dehydratedAppState) {
         <link rel="stylesheet" type="text/css" href="public/css/main.css"/>
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.1.0/introjs.min.css"/>
       </head>
-      <body><div id="app-root">${contents}</div></body>
+      <body>
+        <div id="app">
+          <div id="browser-warning"></div>
+          <div id="app-root">
+            ${contents}
+          </div>
+        </div>
+      </body>
       <script>
         window.dl = window.dl || {};
         window.dl.init__dehydratedAppState = ${dehydratedAppState};
       </script>
+      <script type="text/javascript" src="public/js/browser-warning.js"></script>
       <script type="text/javascript" src="public/js/app.js"></script>
     </html>`
 }
