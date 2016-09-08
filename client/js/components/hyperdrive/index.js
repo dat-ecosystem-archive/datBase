5c616a5f7354692b1cc17c801b690b48cd50339e
karissa <krmckelv@gmail.com>
2016-09-08 18:09:08 +0200

    only override behavior when previewing a file

--- client/js/components/hyperdrive/index.js
+++ client/js/components/hyperdrive/index.js
@@ -23,10 +23,11 @@ if (module.parent) {
     return require('hyperdrive-ui')(null, {root: state.archive.cwd, entries: state.archive.entries}, (ev, entry) => {
       if (entry.type === 'directory') {
         send('archive:update', {root: entry.name})
+        return true
       } else {
         send('preview:file', {archiveKey: state.archive.key, entryName: entry.name}, noop)
+        return false
       }
-      return false
     })
   }
 }
