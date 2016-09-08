f74f0f2ac0dde87dd127c1f9c0266a3b293e0512
Lauren Garcia <lauren.garcia@gmail.com>
2016-09-08 22:57:08 +0200

    update versioning script, set up ?debug=true on router

--- bin/version-assets.js
+++ bin/version-assets.js
@@ -2,26 +2,29 @@
 var fs = require('fs')
 var Version = require('node-version-assets')
 
-// productionize `index.html` by updating asset references
+// productionize `server/page.js` by updating asset references
 // `main.css` -> 'main.min.css'
 // `app.js` -> `app.min.js`
-// re-name original, un-productionized `index.html` to `debug.html`
-var index = fs.readFileSync('index.html', 'utf8')
-fs.renameSync('index.html', 'debug.html')
-var prod = index
+// re-name original, un-productionized `page.js` to `page-debug.html`
+var page = 'server/page.js'
+var pageContents = fs.readFileSync(page, 'utf8')
+fs.renameSync(page, 'server/page-debug.js')
+
+var prod = pageContents
              .replace(/public\/css\/main.css/gmi, 'public/css/main.min.css')
              .replace(/public\/js\/app.js/gmi, 'public/js/app.min.js')
-fs.writeFileSync('index.html', prod, 'utf8')
+fs.writeFileSync(page, prod, 'utf8')
 
 // version `app.min.js` and `main.min.js` by appending md5 hash to filename
-// & update references to files in `index.html`
+// & update references to files in `server/page.js`
 var versionInstance = new Version({
   assets: [
     'public/js/app.min.js',
+    'public/js/browser-warning.js',
     'public/css/main.min.css'
   ],
   grepFiles: [
-    'index.html'
+    page
   ],
   keepOriginal: true,
   keepOldVersions: false
