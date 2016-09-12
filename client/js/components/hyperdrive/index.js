ea9db8eedd1fcbd685a925c8f8a94724b0c82412
karissa <krmckelv@gmail.com>
2016-09-12 18:26:42 +0200

    disable preview until ready

--- client/js/components/hyperdrive/index.js
+++ client/js/components/hyperdrive/index.js
@@ -25,7 +25,7 @@ if (module.parent) {
         send('archive:update', {root: entry.name})
         return true
       } else {
-        send('preview:file', {archiveKey: state.archive.key, entryName: entry.name}, noop)
+        //send('preview:file', {archiveKey: state.archive.key, entryName: entry.name}, noop)
         return false
       }
     })
