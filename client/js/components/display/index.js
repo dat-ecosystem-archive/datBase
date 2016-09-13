ad09741254f2bf250870579b5263a8a019697bc2
karissa <krmckelv@gmail.com>
2016-09-13 11:43:47 +0200

    fix the buggy preview screen

---
+++ client/js/components/display/index.js
@@ -0,0 +1,23 @@
+const html = require('choo/html')
+
+// XXX: server-side data rendering could pull from a cache if we want
+const data = module.parent ? function () { } : require('render-data')
+const display = html`<div id="item"></div>`
+
+module.exports = function (state, prev, send) {
+  const entryName = state.preview.entryName
+  const archive = state.archive.instance
+  if (entryName && archive) {
+    data.render({
+      name: entryName,
+      createReadStream: function () {
+        return archive.createFileReadStream(entryName)
+      }
+    }, display, function (err) {
+      if (err) throw err
+    })
+  }
+
+  console.log(display)
+  return display
+}
