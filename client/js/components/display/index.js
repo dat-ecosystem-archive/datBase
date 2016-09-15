fa6aa8a4dd231cf4adfc6620e378e3ab83dc6936
Lauren Garcia <lauren.garcia@gmail.com>
2016-09-15 21:13:34 +0200

    fix preview display siezures; use new hyperdrive method isEntryDownloaded() as gatekeeper for renderData() call

--- client/js/components/display/index.js
+++ client/js/components/display/index.js
@@ -2,12 +2,18 @@ const html = require('choo/html')
 const fourohfour = require('../../elements/404')
 
 // XXX: server-side data rendering could pull from a cache if we want
-const data = module.parent ? function () { } : require('render-data')
+const renderData = module.parent ? function () { } : require('render-data')
 const display = html`<div id="item"></div>`
 
 module.exports = function (state, prev, send) {
-  const entryName = state.preview.entryName
   const archive = state.archive.instance
+  const entryName = state.preview.entryName
+  const previousEntryName = prev && prev.preview ? prev.preview.entryName : null
+
+  // only render/re-render when the entry name changes!
+  if (!entryName || (entryName === previousEntryName)) {
+    return
+  }
 
   if (state.preview.error) {
     return fourohfour({
@@ -16,26 +22,31 @@ module.exports = function (state, prev, send) {
       link: false
     })
   }
+
   if (entryName && archive) {
-    var done = false
-    var stream = archive.createFileReadStream(entryName)
-    setTimeout(function () {
-      send('preview:update', {error: new Error('Could not find any sources.')})
-      if (!done) stream.destroy()
-    }, 1000)
-    stream.on('data', function (data) {
-      done = true
-    })
-    data.render({
-      name: entryName,
-      createReadStream: function () {
-        return stream
+    archive.get(entryName, function (err, entry) {
+      if (err) {
+        return fourohfour({
+          header: err,
+          body: `${state.preview.entryName} cannot be rendered.`,
+          link: false
+        })
       }
-    }, display, function (error) {
-      if (error) {
-        var message = 'Unsupported filetype'
-        if (error.message === 'premature close') message = 'Could not find any sources.'
-        send('preview:update', {error: new Error(message)})
+      // XXX TODO: we might have to check # of blocks downloaded for large files
+      if (archive.isEntryDownloaded(entry)) {
+        var stream = archive.createFileReadStream(entryName)
+        renderData.render({
+          name: entryName,
+          createReadStream: function () { return stream }
+        }, display, function (error) {
+          if (error) {
+            var message = 'Unsupported filetype'
+            if (error.message === 'premature close') message = 'Could not find any peer sources.'
+            send('preview:update', {error: new Error(message)})
+          }
+        })
+      } else {
+        send('preview:update', {error: new Error('Could not find any peer sources.')})
       }
     })
   }
