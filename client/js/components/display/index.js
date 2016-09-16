d8fea757e0d088b48e41e694538d45300d6cbf86
Lauren Garcia <lauren.garcia@gmail.com>
2016-09-16 16:19:18 +0200

    bump hyperdrive, implement timeout option & callback messaging on archive.get(entry) for preview display

--- client/js/components/display/index.js
+++ client/js/components/display/index.js
@@ -1,21 +1,17 @@
 const html = require('choo/html')
+const loading = require('../../elements/loading')
 const fourohfour = require('../../elements/404')
 
 // XXX: server-side data rendering could pull from a cache if we want
 const renderData = module.parent ? function () { } : require('render-data')
-const display = html`<div id="item"></div>`
+const display = html`<div id="item">${loading()}</div>`
 
 module.exports = function (state, prev, send) {
   const archive = state.archive.instance
   const entryName = state.preview.entryName
   const previousEntryName = prev && prev.preview ? prev.preview.entryName : null
 
-  // only render/re-render when the entry name changes!
-  if (!entryName || (entryName === previousEntryName)) {
-    return
-  }
-
-  if (state.preview.error) {
+  if (state.preview.error && !prev.preview.error) {
     return fourohfour({
       header: state.preview.error.message,
       body: `${state.preview.entryName} cannot be rendered.`,
@@ -23,14 +19,19 @@ module.exports = function (state, prev, send) {
     })
   }
 
+  // only render/re-render when the entry name changes!
+  if (!entryName || (entryName === previousEntryName)) {
+    return
+  }
+
   if (entryName && archive) {
-    archive.get(entryName, function (err, entry) {
+    archive.get(entryName, {timeout: 3000}, function (err, entry) {
       if (err) {
-        return fourohfour({
-          header: err,
-          body: `${state.preview.entryName} cannot be rendered.`,
-          link: false
-        })
+        if (err.code && err.code === 'ETIMEDOUT') {
+          return send('preview:update', {error: new Error('Could not find any peer sources.')})
+        } else {
+          return send('preview:update', {error: new Error(err)})
+        }
       }
       // XXX TODO: we might have to check # of blocks downloaded for large files
       if (archive.isEntryDownloaded(entry)) {
