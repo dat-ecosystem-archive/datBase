7121a23d52b247d48bfb4f432a16059daab26016
karissa <krmckelv@gmail.com>
2016-09-16 18:16:53 +0100

    cleanup

--- client/js/elements/error/index.js
+++ client/js/elements/error/index.js
@@ -2,8 +2,7 @@ var html = require('choo/html')
 
 module.exports = (error) => {
   if (error) {
-    var message = error.message
-    if (message === 'Could not find entry') {
+    if (error.message === 'Could not find entry') {
       return html`<div class="error">
         Add a dat.json or datapackage.json file for more detail. <a href="https://github.com/juliangruber/dat.json" target="_blank">Learn more.</a>
       </div>`
