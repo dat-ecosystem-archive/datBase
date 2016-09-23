c3229b7a8b286966a7cae1e8518e6dbbb3ab11e8
karissa <krmckelv@gmail.com>
2016-09-23 19:47:12 +0200

    update needs a cb

--- client/js/models/archive.js
+++ client/js/models/archive.js
@@ -210,7 +210,7 @@ module.exports = {
         swarm: dat.swarm,
         key: key
       }
-      send('archive:update', newState)
+      send('archive:update', newState, noop)
       return done()
     },
     readFile: function (data, state, send, done) {
