f1ad73800336ee254cf65eab4ebb4bdf9258405d
karissa <krmckelv@gmail.com>
2016-09-26 23:34:18 +0200

    move rendering file display to outside

--- tests/e2e/connect.js
+++ tests/e2e/connect.js
@@ -81,19 +81,10 @@ module.exports = new function () {
       client.setValue('#add-files input[type=file]', path.join(__dirname, '..', 'fixtures', 'dat.json'))
         .expect.element('#fs').text.to.contain('dat.json').before(10000)
     }
-    testCases['render file display'] = (client) => {
-      client.click('.entry.file').pause(3000)
-        .expect.element('#preview').to.have.attribute('class').which.contains('open').before(1000)
-      client.expect.element('.dat-detail.size').text.to.contain('48 B').before(1000)
-    }
   } else {
     testCases['file synced'] = (client) => {
       client
         .expect.element('#fs').text.to.contain('dat.json').before(10000)
-    }
-    testCases['metadata rendered'] = client => {
-      client.expect.element('#title').text.to.contain('hello world').before(1000)
-      client.expect.element('#author').text.to.contain('joe bob').before(1000)
     }
     testCases['create new button properly resets view'] = (client) => {
       client.click('.dat-button--new-dat button').pause(1000)
@@ -115,6 +106,14 @@ module.exports = new function () {
   testCases['metadata rendered'] = client => {
     client.expect.element('#title').text.to.contain('hello world').before(1000)
     client.expect.element('#author').text.to.contain('joe bob').before(1000)
+  }
+
+  testCases['render file display'] = (client) => {
+    client.click('.entry.file').pause(3000)
+      .expect.element('#preview').to.have.attribute('class').which.contains('open').before(1000)
+    client.expect.element('.dat-detail.size').text.to.contain('48 B').before(1000)
+    client.click('.panel-header__close-button').pause(1000)
+      .expect.element('#preview').to.have.attribute('class').not.to.contain('open').before(1000)
   }
 
   testCases.after = (client, done) => {
