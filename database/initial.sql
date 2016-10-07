24ba682d64389b6961eb56d0c7d83cfce3a84e13
karissa <krmckelv@gmail.com>
2016-10-07 19:06:10 +0200

    user is not null on a dat

--- database/initial.sql
+++ database/initial.sql
@@ -12,7 +12,7 @@ CREATE TABLE user (
 
 CREATE TABLE dat (
     id serial PRIMARY KEY,
-    user varchar references users(user),
+    user varchar references users(user) NOT NULL,
     name varchar NOT NULL,
     hash varchar NOT NULL,
     title varchar,
