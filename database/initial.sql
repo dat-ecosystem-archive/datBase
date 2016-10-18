3d1acf88de3b1908bc8919f488cf5a0947e711eb
Karissa McKelvey <karissa@users.noreply.github.com>
2016-10-17 18:25:36 -0700

    remove users table. have auth0 handle it.

--- database/initial.sql
+++ database/initial.sql
@@ -3,20 +3,12 @@ CREATE DATABASE datland;
 
 \c datland;
 
-CREATE TABLE users (
-    id serial PRIMARY KEY,
-    nickname varchar NOT NULL,
-    password varchar NOT NULL,
-    email varchar NOT NULL
-);
-
 CREATE TABLE dat (
     id serial PRIMARY KEY,
     user_id serial references users(id) NOT NULL,
     name varchar NOT NULL,
     hash varchar NOT NULL,
     title varchar,
-    email_verified boolean,
     description text,
     keywords text[]
 );
