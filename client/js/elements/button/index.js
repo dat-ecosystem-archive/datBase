84bfa6534f0164899707c4cf8f59a48713f55dbe
Joe Hand <joe@joeahand.com>
2016-10-21 13:36:23 -0700

    disable downloads for preview files

--- client/js/elements/button/index.js
+++ client/js/elements/button/index.js
@@ -5,12 +5,12 @@ module.exports = (props, click) => {
   if (typeof click === 'function') props.click = click
   var child
   if (props.icon) {
-    child = html`<div class="btn__icon-wrapper"><img class="btn__icon-img" src="${props.icon}" /><span class="btn__icon-text">${props.text}</span></div>`
+    child = html`<div class="btn__icon-wrapper ${props.disabled ? 'disabled' : ''}"><img class="btn__icon-img" src="${props.icon}" /><span class="btn__icon-text">${props.text}</span></div>`
   } else {
     child = props.text
   }
   return html`
-    <button onclick=${props.click} class="${props.klass}">
+    <button onclick=${props.click} class="${props.klass}" ${props.disabled ? 'disabled' : ''}>
       ${child}
     </button>`
 }
