56007a4c2cedb712a761c049d9e64cea5062a43d
Joe Hand <joe@joeahand.com>
2016-10-25 12:39:30 -0600

    separate body scroll from preview scroll

--- client/js/models/preview.js
+++ client/js/models/preview.js
@@ -17,9 +17,11 @@ module.exports = {
       return data
     },
     openPanel: (data, state) => {
+      document.body.classList.add('panel-open') // not ideal
       return {isPanelOpen: true, isLoading: true}
     },
     closePanel: (data, state) => {
+      document.body.classList.remove('panel-open')
       return defaultState
     }
   },
