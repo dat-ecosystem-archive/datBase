cd697f2b4519a369ad1e32f9f3964c8030158f04
karissa <krmckelv@gmail.com>
2016-12-15 12:18:19 -0800

    fix gravatar style

--- client/js/elements/gravatar/index.js
+++ client/js/elements/gravatar/index.js
@@ -4,5 +4,5 @@ const gravatar = require('gravatar')
 module.exports = function (user) {
   if (!user || !user.email) return html``
   var url = gravatar.url(user.email, {s: '200', r: 'pg', d: '404'})
-  return html`<div class="gravatar"><img src="${url}" /></div>`
+  return html`<img class="gravatar" src="${url}" />`
 }
