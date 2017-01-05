43d0c48f33848e214a659b164cf7575f7ad275e6
karissa <krmckelv@gmail.com>
2017-01-04 17:04:56 -0800

    update marketing

--- client/js/pages/landing/index.js
+++ client/js/pages/landing/index.js
@@ -6,7 +6,7 @@ const landingPage = (state, prev, send) => {
     <div class="landing-main">
       <div class="landing-header">
         <img src="/public/img/dat-data-logo.svg" />
-        <p>Share, version, and sync data on the distributed web.</p>
+        <p>Sync data across the distributed web.</p>
         <button href="https://github.com/datproject/dat-desktop">Download the Desktop App</button>
         <div class="row">
         <p><a href="https://datproject.org">About Dat</a> | <a href="https://github.com/datproject/dat">GitHub</a> | <a href="https://twitter.com/dat_project">Twitter</a> | <a href="http://tinyletter.com/datdata">Mailing List</a>
