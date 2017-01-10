b09d3f1b1de70785497adc21d50fd95f35943bc9
Joe Hand <joe@joeahand.com>
2017-01-09 17:34:11 -0800

    start side panel styles

--- client/js/components/auth/user-panel.js
+++ client/js/components/auth/user-panel.js
@@ -12,19 +12,21 @@ module.exports = function (state, prev, send) {
   `
   return html`<div class="user-panel ${prefix} ${state.user.panel}">
     <div class="top-part">
+      <a class="close" href="#" onclick=${() => send('user:panel')}></a>
       ${gravatar(state.user)}
+      ${state.user.email}
     </div>
     <div class="body-part">
     <ul>
       <li>List item #1</li>
       <li>List item #2</li>
     </ul>
-    <button onclick=${() => send('user:logout', {})}>Logout</button>
     </div>
     <div class="footer-part">
-      DatLand version 1.0.0 // TODO: get real version number
-      <span><a href="http://github.com/datproject/datfolder/issues" target="_blank">Report Bug</a> |
-      <a href="http://github.com/datproject/datfolder" target="_blank">Contribute</a></span>
+      <button class="btn btn--large btn--green" onclick=${() => send('user:logout', {})}>LOGOUT</button>
+      <p>DatLand TODO version</p>
+      <p><span><a href="http://github.com/datproject/datfolder/issues" target="_blank">Report Bug</a> |
+      <a href="http://github.com/datproject/datfolder" target="_blank">Contribute</a></span></p>
     </div>
   </div>`
 }
