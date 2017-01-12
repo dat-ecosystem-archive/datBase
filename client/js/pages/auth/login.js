ec4f40912c3188bd0c56451693cf925db2f50815
karissa <krmckelv@gmail.com>
2017-01-11 16:24:19 -0800

    fix bug where /login wouldnt work if not logged in

--- client/js/pages/auth/login.js
+++ client/js/pages/auth/login.js
@@ -2,8 +2,10 @@ const html = require('choo/html')
 const header = require('./../../components/header')
 
 module.exports = (state, prev, send) => {
-  if (!state.user.username) send('user:loginPanel', true)
-  send('user:loginPanel', false)
+  if (!module.parent) {
+    if (!state.user.username) send('user:loginPanel', true)
+    send('user:loginPanel', false)
+  }
   return html`
     <div class="landing">
       ${header(state, prev, send)}
