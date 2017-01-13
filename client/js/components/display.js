818cc34f376cbf3a70d91be47f9791a012718d51
karissa <krmckelv@gmail.com>
2017-01-12 17:44:09 -0800

    fix standard

--- client/js/components/display.js
+++ client/js/components/display.js
@@ -3,11 +3,9 @@ const loading = require('../elements/loading')
 const fourohfour = require('../elements/404')
 
 // XXX: server-side data rendering could pull from a cache if we want
-const renderData = module.parent ? function () { } : require('render-data')
 const display = html`<div id="item">${loading()}</div>`
 
 module.exports = function (state, prev, send) {
-  const archive = state.archive.instance
   const entryName = state.preview.entry && state.preview.entry.name
   const previousEntryName = prev && prev.preview ? prev.preview.entry && prev.preview.entry.name : null
 
@@ -29,25 +27,25 @@ module.exports = function (state, prev, send) {
   return send('preview:update', {error: {message: 'Download the Desktop App', body: 'To view and download files, use the desktop app.'}})
 
   // TODO: make all the below code fetch a data preview via websockets instead
+  //
+  // if (entryName && archive) {
+  //   send('preview:update', {error: {message: 'Looking for peers...', body: 'If it is taking a long time, use the desktop app.'}})
+  //   var stream = archive.createFileReadStream(entryName)
+  //   renderData.render({
+  //     name: entryName,
+  //     createReadStream: function () { return stream }
+  //   }, display, function (error) {
+  //     if (error) {
+  //       var update = {}
+  //       var message = 'Unsupported filetype'
+  //       if (error.message === 'premature close') message = 'Could not find any peer sources.'
+  //       else update.isLoading = false // Allow downloads for unsupported files
+  //       update.error = {message: message}
+  //       return send('preview:update', update)
+  //     }
+  //     send('preview:update', {isLoading: false, error: error})
+  //   })
+  // }
 
-  if (entryName && archive) {
-    send('preview:update', {error: {message: 'Looking for peers...', body: 'If it is taking a long time, use the desktop app.'}})
-    var stream = archive.createFileReadStream(entryName)
-    renderData.render({
-      name: entryName,
-      createReadStream: function () { return stream }
-    }, display, function (error) {
-      if (error) {
-        var update = {}
-        var message = 'Unsupported filetype'
-        if (error.message === 'premature close') message = 'Could not find any peer sources.'
-        else update.isLoading = false // Allow downloads for unsupported files
-        update.error = {message: message}
-        return send('preview:update', update)
-      }
-      send('preview:update', {isLoading: false, error: error})
-    })
-  }
-
-  return display
+  // return display
 }
