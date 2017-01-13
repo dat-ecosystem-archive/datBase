b0c07173198d28654c4e373d6df26d2f9c49226c
Kristina Schneider <me@kristinaschneider.com>
2017-01-13 15:36:31 -0800

    clean up register form #344

--- client/js/components/auth/register.js
+++ client/js/components/auth/register.js
@@ -12,10 +12,33 @@ const login = (state, prev, send) => {
   return html`<div class="register">
     <form onsubmit=${onSubmit}>
       <div class="error">${state.error ? state.error.message : ''}</div>
-      <input type="text" placeholder="Username" name="username" />
-      <input type="text" placeholder="E-mail" name="email" />
-      <input type="password" placeholder="Password" name="password" />
-      <input type="submit" value="Register" />
+      <p>
+        <label for="username" class="dat-input dat-input--icon">
+          <input name="username" type="text" placeholder="Username" class="dat-input__input dat-input__input--icon" />
+          <svg class="dat-input__icon">
+            <use xlink:href="#daticon-happy-dat" />
+          </svg>
+        </label>
+      </p>
+      <p>
+        <label for="email" class="dat-input dat-input--icon">
+          <input name="email" type="email" placeholder="E-mail" class="dat-input__input dat-input__input--icon" />
+          <svg class="dat-input__icon">
+            <use xlink:href="#daticon-letter" />
+          </svg>
+        </label>
+      </p>
+      <p>
+        <label for="password" class="dat-input dat-input--icon">
+          <input name="password" type="email" placeholder="Password" class="dat-input__input dat-input__input--icon" />
+          <svg class="dat-input__icon">
+            <use xlink:href="#daticon-lock" />
+          </svg>
+        </label>
+      </p>
+      <p>
+        <input type="submit" value="Register" class="w100 btn btn--green" />
+      </p>
     </form>
   </div>`
 }
