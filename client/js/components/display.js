a9968230e7f48f71aedce83f05a77e0d9244ad94
Karissa McKelvey <karissa@users.noreply.github.com>
2017-01-12 17:53:10 -0800

    add message to download a client to get data (#378)
    * add message to download desktop app to get data
    
    * fix standard
    
    * fix bug of clicking on the same file twice
    
    * remove unnecessary code

--- client/js/components/display.js
+++ client/js/components/display.js
@@ -1,15 +1,7 @@
-const html = require('choo/html')
-const loading = require('../elements/loading')
 const fourohfour = require('../elements/404')
 
-// XXX: server-side data rendering could pull from a cache if we want
-const renderData = module.parent ? function () { } : require('render-data')
-const display = html`<div id="item">${loading()}</div>`
-
 module.exports = function (state, prev, send) {
-  const archive = state.archive.instance
   const entryName = state.preview.entry && state.preview.entry.name
-  const previousEntryName = prev && prev.preview ? prev.preview.entry && prev.preview.entry.name : null
 
   if (state.preview.error) {
     return fourohfour({
@@ -18,34 +10,5 @@ module.exports = function (state, prev, send) {
       link: false
     })
   }
-
-  // only render/re-render when the entry name changes!
-  if (!entryName) {
-    return
-  } else if (entryName === previousEntryName) {
-    return display
-  }
-
-  send('preview:update', {isLoading: true})
-
-  if (entryName && archive) {
-    send('preview:update', {error: {message: 'Looking for peers...', body: 'If it is taking a long time, use the desktop app.'}})
-    var stream = archive.createFileReadStream(entryName)
-    renderData.render({
-      name: entryName,
-      createReadStream: function () { return stream }
-    }, display, function (error) {
-      if (error) {
-        var update = {}
-        var message = 'Unsupported filetype'
-        if (error.message === 'premature close') message = 'Could not find any peer sources.'
-        else update.isLoading = false // Allow downloads for unsupported files
-        update.error = {message: message}
-        return send('preview:update', update)
-      }
-      send('preview:update', {isLoading: false, error: error})
-    })
-  }
-
-  return display
+  return send('preview:update', {error: {message: 'Can not download at this time.', body: 'To view and download files, use the desktop app or command line tool.'}})
 }
