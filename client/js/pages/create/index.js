131b18bcca926048a4ed899ac85b347ea4ec9c19
karissa <krmckelv@gmail.com>
2017-01-23 16:10:42 -0800

    update more copy

--- client/js/pages/create/index.js
+++ client/js/pages/create/index.js
@@ -6,13 +6,37 @@ const createPage = (state, prev, send) => {
   return html`
     <div>
     ${header(state, prev, send)}
+
+    <section class="section bg-neutral-04" id="terminal">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-6">
+            <h2 class="content-title horizontal-rule">
+              Install Dat
+            </h2>
+            <p>
+            A Dat Archive contains all of the files for your project, including the version history.
+             Dat will watch files as you change them and send them to peers. Install Dat in the terminal using npm.</p>
+            <pre><code>$ npm install -g dat
+$ dat share path/to/my/data</code></pre>
+            <a href="http://docs.datproject.org" target="_blank" class="">
+            See detailed instructions
+            </a>
+          </div>
+          <div class="col-xs-12 col-sm-6">
+            <img src="/public/img/dat-terminal.svg" class="screenshot" />
+          </div>
+        </div>
+      </div>
+    </section>
       <section class="section">
         <div class="container">
           <div class="row">
             <div class="col-xs-12 col-sm-5">
-              <h1 class="f1 content-title horizontal-rule">Using Dat</h1>
-              <p class="measure-wide">A Dat Archive contains all of the files for your project, including the version history.
-              Create your own Dat using either our Desktop App or the CLI.</p>
+              <h2 class="f1 content-title horizontal-rule">All the ways to use Dat</h2>
+              <p class="measure-wide">
+              Share your link through the web using the 'Open Dat Link' box.
+              Soon you'll be able to share files with the Desktop App, coming soon to all platforms.</p>
             </div>
             <div class="col-xs-12 col-sm-7">
               <div class="row">
@@ -41,31 +65,11 @@ const createPage = (state, prev, send) => {
                 <a href="#terminal">
                   <div class="bg-neutral hover-bg-neutral-80 mb4 p3 block-create">
                     <img src="/public/img/terminal-icon.svg" class="block-create__icon" />
-                    <p class="mb0 f6 lh-title color-white">On the terminal</p>
+                    <p class="mb0 f6 lh-title color-white">In the Terminal</p>
                   </div>
                   </a>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="section bg-neutral-04" id="terminal">
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-12 col-sm-6">
-              <img src="/public/img/dat-terminal.svg" class="screenshot" />
-            </div>
-            <div class="col-xs-12 col-sm-6">
-              <h2 class="content-title horizontal-rule">
-                On the terminal
-              </h2>
-              <p>Use Dat in the terminal. Dat will watch and sync files as you change them.</p>
-              <pre><code>$ npm install -g dat
-$ dat share path/to/my/data</code></pre>
-              <a href="http://docs.datproject.org" target="_blank" class="btn btn--green">
-                Read full documentation
-              </a>
             </div>
           </div>
         </div>
@@ -77,8 +81,7 @@ $ dat share path/to/my/data</code></pre>
               <h2 class="content-title color-white horizontal-rule">
                 Dat Desktop
               </h2>
-              <p class="color-white">Sync data using our desktop app.</p>
-              <p class="color-white">To manage multiple dats on your desktop machine, use the app, coming soon for all platforms. </p>
+              <p class="color-white">To manage multiple Dats on your desktop machine, use the app, coming soon for all platforms. </p>
               <a href="http://github.com/datproject/dat-desktop" target="_blank" class="btn btn--green">
                 View on GitHub
               </a>
