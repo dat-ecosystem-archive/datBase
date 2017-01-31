1d859dec50e56f9a50d99df092b610c01b664457
karissa <krmckelv@gmail.com>
2017-01-31 13:10:50 -0800

    add leveldb cache for metadata

--- server/haus.js
+++ server/haus.js
@@ -1,27 +1,14 @@
-const encoding = require('dat-encoding')
-var level = require('level')
 var hyperdrive = require('hyperdrive')
-var hyperhealth = require('hyperhealth')
 
 module.exports = Haus
 
-function Haus (key, opts) {
-  if (!(this instanceof Haus)) return new Haus(key, opts)
-  if (typeof opts === 'function') return new Haus(key, {}, opts)
+function Haus (opts) {
+  if (!(this instanceof Haus)) return new Haus(opts)
   if (!opts) opts = {}
-  key = encoding.toBuf(key)
-  this.db = level()
-  this.drive = hyperdrive(this.db)
-  this.archive = this.drive.createArchive(key, {sparse: true, live: true})
-  this.health = hyperhealth(this.archive, opts)
-  this.swarm = this.health.swarm
+  this.drive = hyperdrive(opts.cachedb)
 }
 
 Haus.prototype.close = function (cb) {
   var self = this
-  this.swarm.close(function () {
-    self.archive.close(function () {
-      self.db.close(cb)
-    })
-  })
+  self.drive.close(cb)
 }
