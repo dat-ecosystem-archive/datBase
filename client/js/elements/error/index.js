61086e9cc44fa14f76743bf005669cd27184da19
karissa <krmckelv@gmail.com>
2017-01-31 13:17:07 -0800

    tiny cosmetic fixes

--- client/js/elements/error/index.js
+++ client/js/elements/error/index.js
@@ -2,7 +2,7 @@ var html = require('choo/html')
 
 module.exports = (error) => {
   if (error) {
-    if (error.message === 'no metadata') {
+    if (error.message === 'no metadata' || error.message === 'Could not find entry') {
       return html`<div>
         Add a dat.json or datapackage.json file for more detail. <a href="https://github.com/juliangruber/dat.json" target="_blank">Learn more.</a>
       </div>`
