752b9c80a0b6501e74ef6f3eccedf8f6f6dff141
karissa <krmckelv@gmail.com>
2017-01-31 13:12:07 -0800

    Revert "use leveldb for caching"
    This reverts commit aaee32fa204c26f5714f1f0217536e9072cf3bbb.

--- server/haus.js
+++ server/haus.js
@@ -1,5 +1,5 @@
 const encoding = require('dat-encoding')
-var level = require('level')
+var memdb = require('memdb')
 var hyperdrive = require('hyperdrive')
 var hyperhealth = require('hyperhealth')
 
@@ -10,7 +10,7 @@ function Haus (key, opts) {
   if (typeof opts === 'function') return new Haus(key, {}, opts)
   if (!opts) opts = {}
   key = encoding.toBuf(key)
-  this.db = level(__dirname, '.datcache')
+  this.db = memdb()
   this.drive = hyperdrive(this.db)
   this.archive = this.drive.createArchive(key, {sparse: true, live: true})
   this.health = hyperhealth(this.archive, opts)
