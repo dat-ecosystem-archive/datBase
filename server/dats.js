c9ba91ce67c43080685d93721dd125c789f92a8f
karissa <krmckelv@gmail.com>
2017-02-10 15:51:44 -0800

    add getting filename route

--- server/dats.js
+++ server/dats.js
@@ -1,7 +1,5 @@
 const Archiver = require('hypercore-archiver')
 const mkdirp = require('mkdirp')
-const path = require('path')
-const level = require('level')
 const encoding = require('dat-encoding')
 const hyperdrive = require('hyperdrive')
 const archiverServer = require('archiver-server')
@@ -13,20 +11,20 @@ function Dats (dir) {
   mkdirp.sync(dir)
   this.archiver = Archiver(dir)
   this.server = archiverServer(this.archiver, {dontShare: true, dht: false})
-  this.drive = hyperdrive(level(path.join(dir, 'hyperdrive')))
+  this.drive = hyperdrive(this.archiver.db)
   this.archives = {}
 }
 
-Dats.prototype.add = function (key, cb) {
+Dats.prototype.get = function (key, cb) {
   var self = this
   key = encoding.toStr(key)
   var buf = encoding.toBuf(key)
   if (self.archives[key]) return cb(null, self.archives[key])
-  self.archiver.add(buf, {content: false}, function (err) {
+  self.archiver.add(buf, function (err) {
     if (err) return cb(err)
     self.archiver.get(buf, function (err, metadata, content) {
       if (err) return cb(err)
-      var archive = self.drive.createArchive(buf, {metadata: metadata, sparse: true})
+      var archive = self.drive.createArchive(buf, {metadata: metadata, content: content})
       self.archives[key] = archive
       return cb(null, archive)
     })
