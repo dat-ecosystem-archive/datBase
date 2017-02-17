d5f7093e362c11ee8787dff1e61a94dff20ed535
karissa <krmckelv@gmail.com>
2017-02-17 11:48:35 -0800

    create better test cleanup

--- tests/new-dat.js
+++ tests/new-dat.js
@@ -2,14 +2,16 @@
 var fs = require('fs')
 var Dat = require('dat-node')
 var path = require('path')
+var rimraf = require('rimraf')
 
 var loc = path.join(__dirname, 'fixtures')
+rimraf(path.join(loc, '.dat'), function () {
+  Dat(loc, function (err, dat) {
+    if (err) throw err
+    dat.importFiles(function () {
+    })
 
-Dat(loc, function (err, dat) {
-  if (err) throw err
-  dat.importFiles(function () {
+    dat.joinNetwork()
+    fs.writeFile(path.join(__dirname, 'key.txt'), dat.key.toString('hex'))
   })
-
-  dat.joinNetwork()
-  fs.writeFile(path.join(__dirname, 'key.txt'), dat.key.toString('hex'))
 })
