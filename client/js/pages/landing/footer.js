c2a499b86291bf98686fe7c81c8be3a6161109ff
karissa <krmckelv@gmail.com>
2017-02-22 00:27:58 -0800

    update footer and splash

--- client/js/pages/landing/footer.js
+++ client/js/pages/landing/footer.js
@@ -24,7 +24,7 @@ module.exports = function (state, prev, send) {
           <h4 class="footer-heading horizontal-rule-footer">Connect</h4>
           <ul class="footer-nav-list">
             <li><a href="https://twitter.com/dat_project">Twitter</a></li>
-            <li><a href="https://github.com/datproject/dat">GitHub</a></li>
+            <li><a href="https://github.com/datproject/datproject">GitHub</a></li>
             <li>
               <a href="https://tinyletter.com/datdata" target="_blank">Newsletter </a>
             </li>
@@ -32,7 +32,7 @@ module.exports = function (state, prev, send) {
         </div>
       </div>
       <p class="footer-credits">
-        <strong>Dat</strong> 2017 &bull; Page source on <a href="https://github.com/datproject/website">github</a>
+        <strong>Dat</strong> 2017 &bull; Page source on <a href="https://github.com/datproject/datproject.org">github</a>
       </p>
     </div>
   </footer>
