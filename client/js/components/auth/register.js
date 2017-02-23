39ffe7cd632432fbbbc2bce5fe5459b6c6021d41
karissa <krmckelv@gmail.com>
2017-02-22 18:38:14 -0800

    add login/register buttons to top right

--- client/js/components/auth/register.js
+++ client/js/components/auth/register.js
@@ -1,5 +1,43 @@
 const html = require('choo/html')
+const css = require('sheetify')
 const form = require('get-form-data')
+
+var prefix = css`
+  :host {
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+    background-color: rgba(41,54,72,.8);
+    .modal {
+      min-width: 20rem;
+      min-width: 25rem;
+      padding: 2rem 2.5rem 2rem;
+      background-color: white;
+      box-shadow: 0 1.2rem 2.4rem rgba(0,0,0,.5);
+    }
+    form {
+      width: 16rem;
+      margin-left: auto;
+      margin-right: auto;
+    }
+    label, input {
+      width: 100%;
+      text-transform: none;
+    }
+    .x {
+      top: 0;
+      right: 0;
+      position: absolute;
+      margin: 5px;
+    }
+  }
+`
 
 const register = (state, prev, send) => {
   function onSubmit (e) {
@@ -9,9 +47,10 @@ const register = (state, prev, send) => {
     return false
   }
 
-  return html`<div class="register">
-    <div class="mw5 pv5 center">
-      <h1 class="f4">Create a New Account</h1>
+  return html`<div class="register ${state.user.register} ${prefix}">
+    <div class="relative flex flex-column justify-center modal">
+      <h3 class="f4">Create a New Accoun</h3>
+      <a class="x" onclick=${() => send('user:registerPanel', false)}>X</a>
       <form onsubmit=${onSubmit}>
         <div class="error">${state.error ? state.error.message : ''}</div>
         <p>
