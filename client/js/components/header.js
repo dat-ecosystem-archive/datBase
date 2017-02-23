a1be6c0d014218c60cec181d5a4b00f88ef6eb55
karissa <krmckelv@gmail.com>
2017-02-22 18:38:14 -0800

    add login/register buttons to top right

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -1,6 +1,7 @@
 const html = require('choo/html')
 const panel = require('./auth/user-panel')
 const login = require('./auth/login')
+const register = require('./auth/register')
 const loginButton = require('./login-button')
 const button = require('../elements/button')
 const importButton = require('../elements/import-button')
@@ -9,6 +10,7 @@ const message = require('../elements/message')
 const header = (state, prev, send) => {
   return html`<div>
     ${panel(state, prev, send)}
+    ${register(state, prev, send)}
     ${login(state, prev, send)}
     <header class="site-header">
       ${message(state.message)}
