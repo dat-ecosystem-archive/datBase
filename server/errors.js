e959e1567f5f66e84d8e0c81e8d331b6fd09a338
karissa <krmckelv@gmail.com>
2017-03-13 18:20:55 +0100

    Give better error message when email is already taken.

--- server/errors.js
+++ server/errors.js
@@ -2,7 +2,8 @@ var list = {
   'UNIQUE constraint failed: dats.name, dats.user_id': 'A dat already exists with that name on that account.',
   'UNIQUE constraint failed: users.username': 'A user already exists with that username.',
   'UNIQUE constraint failed: users.email': 'A user already exists with that email.',
-  'Account not verified': 'Incorrect email and password.'
+  'Account not verified': 'Incorrect email and password.',
+  'Cannot create account with that email address': 'Account with that email already exists.'
 }
 
 module.exports = {
