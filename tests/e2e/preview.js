f2a960a529245d7e5f0a4394dfed45e12ce1318e
karissa <krmckelv@gmail.com>
2017-03-13 21:32:46 +0100

    add preview test

---
+++ tests/e2e/preview.js
@@ -0,0 +1,20 @@
+var fs = require('fs')
+var path = require('path')
+var testServer = process.env.TEST_SERVER || 'https://datproject.org'
+
+module.exports = new function () {
+  var key = fs.readFileSync(path.join(__dirname, '..', 'key.txt')).toString()
+  var testCases = this
+  testCases['Preview file when clicking on it'] = (client) => {
+    client
+      .url(testServer + '/install')
+      .setValue("input[name='import-dat']", key)
+    client.keys(client.Keys.ENTER, function (done) {
+      client
+        .expect.element('#fs').text.to.contain('dat.json').before(5000)
+      client.element('.entry')[0].click()
+      client.element('.panel-title').text.to.contain('dat.json')
+    })
+  }
+
+}
