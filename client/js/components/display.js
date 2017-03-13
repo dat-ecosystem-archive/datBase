55c6333f2eacc8202a5f3fbd538c5ee3bc29d246
karissa <krmckelv@gmail.com>
2017-03-13 20:46:58 +0100

    remove console.logs

--- client/js/components/display.js
+++ client/js/components/display.js
@@ -1,16 +1,14 @@
 const fourohfour = require('../elements/404')
-const loading = require('../elements/loading')
 const http = require('nets')
 const html = require('choo/html')
 const from = require('from2')
 
 const renderData = module.parent ? function () { } : require('render-data')
-const display = html`<div id="item">${loading()}</div>`
+const display = html`<div id="item"></div>`
 
 module.exports = function (state, prev, send) {
   const entryName = state.preview.entry && state.preview.entry.name
   const previousEntryName = prev && prev.preview ? prev.preview.entry && prev.preview.entry.name : null
-  console.log('redisplaying')
 
   if (state.preview.error) {
     return fourohfour({
@@ -20,17 +18,15 @@ module.exports = function (state, prev, send) {
     })
   }
 
-  if (!entryName || (entryName === previousEntryName)) return display
-  console.log('loading', entryName, previousEntryName)
+  if (!entryName) return
+  if (entryName === previousEntryName) return display
 
   send('preview:update', {error: {message: 'Loading...', body: ''}})
   http({url: `/dat/${state.archive.key}/${entryName}`, method: 'GET'}, function (err, resp, file) {
     if (err) throw err
-    console.log('http response', resp, file)
     renderData.render({
       name: entryName,
       createReadStream: function () {
-        console.log('file')
         return from([file])
       }
     }, display, function (err) {
@@ -39,7 +35,6 @@ module.exports = function (state, prev, send) {
         var message = 'Unsupported filetype'
         update.isLoading = false // Allow downloads for unsupported files
         update.error = {message: message}
-        console.log('hi')
         return send('preview:update', update)
       }
       send('preview:update', {isLoading: false, error: err})
