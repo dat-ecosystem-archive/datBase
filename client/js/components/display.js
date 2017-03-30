09ed45bb01d03aa2e9c3243e44a013e6f40e3398
Chia-liang Kao <clkao@clkao.org>
2017-03-30 14:40:06 +0800

    Fix previewing filename containing "#"

--- client/js/components/display.js
+++ client/js/components/display.js
@@ -33,7 +33,8 @@ module.exports = function (state, prev, send) {
     body: 'Please wait…',
     icon: 'loader'
   }})
-  http({url: `/dat/${state.archive.key}/${entryName}`, method: 'GET'}, function (err, resp, file) {
+  // proper escape is done, but # is special
+  http({url: `/dat/${state.archive.key}/${entryName.replace(/#/g, '%23')}`, method: 'GET'}, function (err, resp, file) {
     if (err) return send('preview:update', {error: err})
     renderData.render({
       name: entryName,
