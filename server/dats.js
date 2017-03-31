4e5fb970465706daa69df5ce337a55742f07a3e2
Karissa McKelvey <karissa@users.noreply.github.com>
2017-03-31 16:40:12 -0700

    Re enable preview (#508)
    * add random-access-page-files
    
    * Revert "disable file preview until caching is sorted"
    
    This reverts commit 4eba2f82dc929f21850812affe7f9a1ed0e23603.
    
    * re-enable preview test

--- server/dats.js
+++ server/dats.js
@@ -2,6 +2,7 @@ const Archiver = require('hypercore-archiver')
 const mkdirp = require('mkdirp')
 const encoding = require('dat-encoding')
 const hyperdrive = require('hyperdrive')
+const pages = require('random-access-page-files')
 const Swarm = require('discovery-swarm')
 const swarmDefaults = require('datland-swarm-defaults')
 const hyperhttp = require('hyperdrive-http')
@@ -12,7 +13,7 @@ module.exports = Dats
 function Dats (dir) {
   if (!(this instanceof Dats)) return new Dats(dir)
   mkdirp.sync(dir)
-  this.archiver = Archiver({dir: dir, sparse: true})
+  this.archiver = Archiver({dir: dir, sparse: true, storage: pages})
   this.swarm = createSwarm(this.archiver)
   this.drive = hyperdrive(this.archiver.db)
   this.archives = {}
@@ -24,13 +25,15 @@ Dats.prototype.get = function (key, cb) {
   key = encoding.toStr(key)
   var buf = encoding.toBuf(key)
   if (self.archives[key]) return cb(null, self.archives[key])
-  self.archiver.add(buf, {content: false}, function (err) {
+  self.archiver.add(buf, {content: true}, function (err) {
     if (err) return cb(err)
     self.archiver.get(buf, function (err, metadata, content) {
       if (err) return cb(err)
-      var archive = self.drive.createArchive(buf, {metadata: metadata, content: content})
-      self.archives[key] = archive
-      return cb(null, archive)
+      if (content) {
+        var archive = self.drive.createArchive(buf, {metadata: metadata, content: content})
+        self.archives[key] = archive
+        return cb(null, archive)
+      }
     })
   })
 }
