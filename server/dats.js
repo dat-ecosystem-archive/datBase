3d91962b825640060554bf9620e571af96761ad8
karissa <krmckelv@gmail.com>
2017-04-14 16:39:55 -0500

    more updates for sleep

--- server/dats.js
+++ server/dats.js
@@ -3,7 +3,6 @@ const mkdirp = require('mkdirp')
 const encoding = require('dat-encoding')
 const hyperdrive = require('hyperdrive')
 const ram = require('random-access-memory')
-const raf = require('random-access-file')
 const Swarm = require('discovery-swarm')
 const swarmDefaults = require('datland-swarm-defaults')
 
@@ -12,7 +11,7 @@ module.exports = Dats
 function Dats (dir) {
   if (!(this instanceof Dats)) return new Dats(dir)
   mkdirp.sync(dir)
-  this.archiver = Archiver({dir: dir, sparse: true, storage: raf})
+  this.archiver = Archiver({dir: dir})
   this.swarm = createSwarm(this.archiver)
   this.archives = {}
 }
@@ -21,28 +20,18 @@ Dats.prototype.get = function (key, cb) {
   var self = this
   key = encoding.toStr(key)
   var buf = encoding.toBuf(key)
-  self.archiver.add(buf, {content: true}, function (err) {
+  self.archiver.add(buf, function (err) {
     if (err) return cb(err)
     self.archiver.get(buf, function (err, metadata, content) {
       if (err) return cb(err)
-      if (content) {
-        var archive = hyperdrive(ram, buf, {metadata: metadata, content: content})
-        return cb(null, archive)
-      }
+      var archive = hyperdrive(ram, buf, {metadata: metadata, content: content})
+      return cb(null, archive)
     })
-  })
-}
-
-Dats.prototype.entries = function (archive, cb) {
-  archive.readdir(function (err, entries) {
-    if (err) return cb(err)
-    cb(null, entries)
   })
 }
 
 Dats.prototype.metadata = function (archive, opts, cb) {
   if (typeof opts === 'function') return this.metadata(archive, {}, opts)
-  var self = this
   var dat
   if (!archive.content) dat = {}
   else {
@@ -67,16 +56,13 @@ Dats.prototype.metadata = function (archive, opts, cb) {
     return cb(err, dat)
   }
 
-  self.entries(archive, function (err, entries) {
+  archive.readdir('/', function (err, entries) {
     dat.entries = entries
+    console.log(dat.entries)
     if (err || cancelled) return done(err, dat)
     var filename = 'dat.json'
-    archive.get(filename, function (err, entry) {
-      if (err || cancelled) {
-        archive.content.get(0, function () {
-          return done(err, dat)
-        })
-      }
+    archive.stat(filename, function (err, entry) {
+      if (err || cancelled) return done(null, dat)
       archive.readFile(filename, function (err, metadata) {
         if (err || cancelled) return done(err, dat)
         try {
